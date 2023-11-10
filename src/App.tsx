d4c7fa43aa48b8d2b97034b885949e2fcdfdb59c
Heonyoung Kim <altenull@gmail.com>
2023-11-10 17:11:56 +0900

    feat: add react base files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  // useEffect(() => {
+  //   // TODO: Update state when json file has changed.
+  //   window.addEventListener('message', (event) => {
+  //     // const jsonData = event.data.json;
+  //     // const { nodes, edges } = parser(jsonData);
+  //   });
+  // }, []);
+
+  return <h1>This is App.tsx file.</h1>;
+};
+
+export default App;
