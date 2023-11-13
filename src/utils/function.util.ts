c6fc1938dd16efc8d0e5c4ea1872ad251784b9a2
Heonyoung Kim <altenull@gmail.com>
2023-11-13 22:08:21 +0900

    refactor: remove `noop` util function and rule out `@typescript-eslint/no-empty-function` from eslint

--- src/utils/function.util.ts
+++ src/utils/function.util.ts
@@ -1,7 +1,5 @@
 export const tuple = <T extends string[]>(...args: T) => args;
 
-export const noop = () => {};
-
 /**
  * Returns true if argument is a 'function' and false otherwise.
  * @param {unknown} v - the value to check
